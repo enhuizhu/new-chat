cb301753e251a5c34732bed9f13fbdf3f47537b7
enhuizhu <zhuen2000@163.com>
2019-01-30 15:05:26 +0000

    add build to the repository

---
+++ build/precache-manifest.f6b30716c7d7eb497870963e731392e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5ce0a577ad773a1682c5",
+    "url": "/static/js/main.5ce0a577.chunk.js"
+  },
+  {
+    "revision": "5f7d15e4f07cdd34a675",
+    "url": "/static/js/1.5f7d15e4.chunk.js"
+  },
+  {
+    "revision": "5ce0a577ad773a1682c5",
+    "url": "/static/css/main.51c20d68.chunk.css"
+  },
+  {
+    "revision": "68dad8d5bdaf9f75ef9ec61be264964c",
+    "url": "/index.html"
+  }
+];
